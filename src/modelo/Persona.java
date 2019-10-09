ed6ff85cdfb86ca92291e4e3f1aacbd48febae33
Estudiante <Estudiante@S106-PC19>
2019-10-09 09:50:19 -0500

    Inicio

---
+++ src/modelo/Persona.java
@@ -0,0 +1,195 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Persona {
+    
+    private int id;
+    private String cedula;
+    private String primernombre;
+    private String segundonombre;
+    private String primerapellido;
+    private String segundoapellido;
+    private String sexo;
+    private String correo;
+    private String pais;
+    private String departamento;
+    private String direccion;
+    private String telefono;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the cedula
+     */
+    public String getCedula() {
+        return cedula;
+    }
+
+    /**
+     * @param cedula the cedula to set
+     */
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    /**
+     * @return the primernombre
+     */
+    public String getPrimernombre() {
+        return primernombre;
+    }
+
+    /**
+     * @param primernombre the primernombre to set
+     */
+    public void setPrimernombre(String primernombre) {
+        this.primernombre = primernombre;
+    }
+
+    /**
+     * @return the segundonombre
+     */
+    public String getSegundonombre() {
+        return segundonombre;
+    }
+
+    /**
+     * @param segundonombre the segundonombre to set
+     */
+    public void setSegundonombre(String segundonombre) {
+        this.segundonombre = segundonombre;
+    }
+
+    /**
+     * @return the primerapellido
+     */
+    public String getPrimerapellido() {
+        return primerapellido;
+    }
+
+    /**
+     * @param primerapellido the primerapellido to set
+     */
+    public void setPrimerapellido(String primerapellido) {
+        this.primerapellido = primerapellido;
+    }
+
+    /**
+     * @return the segundoapellido
+     */
+    public String getSegundoapellido() {
+        return segundoapellido;
+    }
+
+    /**
+     * @param segundoapellido the segundoapellido to set
+     */
+    public void setSegundoapellido(String segundoapellido) {
+        this.segundoapellido = segundoapellido;
+    }
+
+    /**
+     * @return the sexo
+     */
+    public String getSexo() {
+        return sexo;
+    }
+
+    /**
+     * @param sexo the sexo to set
+     */
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    /**
+     * @return the correo
+     */
+    public String getCorreo() {
+        return correo;
+    }
+
+    /**
+     * @param correo the correo to set
+     */
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    /**
+     * @return the pais
+     */
+    public String getPais() {
+        return pais;
+    }
+
+    /**
+     * @param pais the pais to set
+     */
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    /**
+     * @return the departamento
+     */
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    /**
+     * @param departamento the departamento to set
+     */
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    /**
+     * @return the direccion
+     */
+    public String getDireccion() {
+        return direccion;
+    }
+
+    /**
+     * @param direccion the direccion to set
+     */
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    /**
+     * @return the telefono
+     */
+    public String getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * @param telefono the telefono to set
+     */
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+}
